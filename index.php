aadf30f50038b4ce6f762f79228b80d3bd8537c3
Viktor <34343728+viksj950@users.noreply.github.com>
2017-12-08 13:53:25 +0100

    Add files via upload
    ver 1.5

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?PHP
+
+	include("connect.php");
+
+	echo "<form method = 'post' action='search_satser.php'>
+	<input type='text' name='search'>
+	<input type='submit' value='Sök på Legosats'>
+	</form>";
+	
+	echo "<form method = 'post' action='index.php'>
+	<input type='submit' value='Gå tillbaka'>
+	</form>";
+
+echo "<img src='index.png' alt = 'kebba'>";
+	
+?>
+
+
---
+++ search_satser.php
@@ -0,0 +1,68 @@
+<?PHP
+
+
+	include('connect.php');
+
+	echo "<form method = 'post' action='search_satser.php'>
+	<input type='text' name='search'>
+	<input type='submit' value='Sök på annan legosats'>
+	</form>";
+	echo "<form method = 'post' action='index.php'>
+	<input type='submit' value='Gå tillbaka'>
+	</form>";
+
+	$search = strip_tags($_POST["search"]); //lägg till real_escape string för säkerhet senare
+
+	$Setname_search = mysqli_query($connection,	"SELECT * FROM sets WHERE Setname = '$search'"); //Lägg till kod som hanterar duplikantor av samma namn och ger någon varningsruta
+
+	
+	$convert_nametoID = mysqli_query($connection, "SELECT SetID, Year FROM sets WHERE Setname = '$search'");
+	 
+	 
+	 
+	while($row = mysqli_fetch_array($Setname_search)) //Sökning på sats. OBS!: kan ge mer än ett resultat
+	{	
+		$result_sats_name = $row['Setname'];
+		$result_sats_Year = $row['Year'];
+		
+	
+
+	while($row3 = mysqli_fetch_array($convert_nametoID)) //Konversion till ID från Setname
+		{	
+		$result_setID = $row3['SetID'];
+		
+			echo "<table><tr>";
+			echo "<th>Sats</th>";
+			echo "<th>Utgiven</th>";
+			echo "<th>SetID</th></tr>";
+			
+			echo "<tr><td>". $result_sats_name."</td>";
+			echo "<td>".$result_sats_Year. "</td>";
+			echo "<td>".$result_setID."</td>";
+			echo "<br></tr></table>";
+			
+			$get_parts = mysqli_query($connection, "SELECT * FROM inventory
+			WHERE SetID = '$result_setID'");
+		
+		while($row2 = mysqli_fetch_array($get_parts)) //Bitarna som är kopplade till setID
+		{	
+			$result_partsID = $row2['ItemID'];
+			$results_partsColor = $row2['ColorID'];
+			$results_partsQuantity = $row2['Quantity'];
+			
+			echo "<table>";
+			echo "PartID:".$result_partsID;
+			echo "Color:".$results_partsColor;
+			echo "Quanitity:".$results_partsQuantity;
+			echo "</table><br>";	
+			
+		}
+		}
+	}
+	
+
+	
+?>
+
+
+
