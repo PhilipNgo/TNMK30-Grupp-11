afeb25cf56df3286a994a70e96b9cbd0e2b87dcc
Viktor <34343728+viksj950@users.noreply.github.com>
2017-12-11 10:57:59 +0100

    Add files via upload
    ver 1.7

--- connect.php
+++ connect.php
@@ -13,7 +13,7 @@ $connection	=	mysqli_connect("mysql.itn.liu.se", "lego" , "" , "lego"); //koppla
 	
 	if ($connection->connect_error)
 	{
-		die("Connection failed: " . $conn->connect_error);
+		die("Connection failed: " . $connection->connect_error);
 	} 
 
 	?>
--- search_satser.php
+++ search_satser.php
@@ -13,25 +13,26 @@
 
 	$search = ($_POST["search"]); //lägg till real_escape string för säkerhet senare
 
-	$Setname_search = mysqli_query($connection,	"SELECT * FROM sets WHERE Setname = '$search'"); //Lägg till kod som hanterar duplikanter av samma namn och ger någon varningsruta
+	$Setname_search = mysqli_query($connection,	"SELECT * FROM sets WHERE Setname = '$search' OR SetID = '$search'"); //Lägg till kod som hanterar duplikanter av samma namn och ger någon varningsruta
 
 	
-	$convert_nametoID = mysqli_query($connection, "SELECT SetID, Year FROM sets WHERE Setname = '$search'");
+	//$convert_nametoID = mysqli_query($connection, "SELECT SetID, Year FROM sets WHERE Setname = '$search'");
 	 
 	 
 	if (mysqli_num_rows($Setname_search) === 0) { 
-		echo "Inga resultat!";
+		echo "<div id='Nope'>'".$search."' gav inga resultat!</div>";
 	}
 	else
 	{
 	while($row = mysqli_fetch_array($Setname_search)) //Sökning på sats. OBS!: kan ge mer än ett resultat
 	{	
 		
-	while($row3 = mysqli_fetch_array($convert_nametoID)) //Konversion till ID från Setname
-		{	
-		$result_setID = $row3['SetID'];
-		$result_sats_name = $row['Setname'];
+		//vad menas med uppgiften
+		
+		$result_setID = $row['SetID'];
 		$result_sats_Year = $row['Year'];
+		$result_sats_name = $row['Setname'];
+		
 		
 			echo "<table><tr>";
 			echo "<th>Sats</th>";
@@ -46,20 +47,20 @@
 			$get_parts = mysqli_query($connection, "SELECT * FROM inventory
 			WHERE SetID = '$result_setID'");
 		
-		while($row2 = mysqli_fetch_array($get_parts)) //Bitarna som är kopplade till setID
-		{	
-			$result_partsID = $row2['ItemID'];
-			$results_partsColor = $row2['ColorID'];
-			$results_partsQuantity = $row2['Quantity'];
-			
-			echo "<table>";
-			echo "PartID:".$result_partsID;
-			echo "Color:".$results_partsColor;
-			echo "Quanitity:".$results_partsQuantity;
-			echo "</table><br>";	
-			
-		}
-		}
+				while($row2 = mysqli_fetch_array($get_parts)) //Bitarna som är kopplade till setID
+				{	
+					$result_partsID = $row2['ItemID'];
+					$results_partsColor = $row2['ColorID'];
+					$results_partsQuantity = $row2['Quantity'];
+					
+					echo "<table>";
+					echo "PartID:".$result_partsID;
+					echo "Color:".$results_partsColor;
+					echo "Quanitity:".$results_partsQuantity;
+					echo "</table><br>";	
+					
+				}
+		
 	}
 }
 
